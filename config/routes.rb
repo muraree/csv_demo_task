1b034d36335051f64c4754d4c1c44a953cd6e05a
muraree <murareelalsahu@gmail.com>
2021-10-14 13:31:18 +0530

    Minor change

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "vehicles#index"
 
-
-  resource :vehicles do
+  resources :vehicles do
     collection { post :import }
     collection { get  :download_csv }
   end
